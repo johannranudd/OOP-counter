213e16a2f1356a83971d54b546d04683709bdb96
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-09-01 15:48:03 +0200

    srdt

--- js/script.js
+++ js/script.js
@@ -1,15 +1,46 @@
-// window.addEventListener("DOMContentLoaded", firstCounterVar)
-// window.addEventListener("DOMContentLoaded", secondCounterVar)
-const firstCounterVal = document.querySelector('.first-counter .value');
-const secondCounterVal = document.querySelector('.second-counter .value');
+
+// const firstCounterVal = document.querySelector('.first-counter');
+// const secondCounterVal = document.querySelector('.second-counter .value');
+
+function getElement(selection) {
+    const element = document.querySelector(selection);
+    if (element) {
+        return element;
+    } else {
+        throw new Error(`please check "${selection}" selector, no such element exists`)
+    }
+}
 
 function Counter(element, value) {
     // console.log(element, value);
-    element.textContent = value;
+    // element.children[1].textContent = value;
+    this.value = value;
+    this.counter = element;
+    this.resetBtn = element.querySelector('.reset');
+    this.increaseBtn = element.querySelector('.increase');
+    this.decreaseBtn = element.querySelector('.decrease');    
+    this.valueDOM = element.querySelector('.value');
+    this.valueDOM.textContent = this.value;
+
+    this.resetBtn.addEventListener("click", function() {
+        firstCounter.reset();
+    })
 }
 
-window.addEventListener("DOMContentLoaded", function() {
-    new Counter(firstCounterVal, 100)
-    new Counter(secondCounterVal, 200)
-})
+Counter.prototype.increase = function() {
+    this.value++;
+    this.valueDOM.textContent = this.value;
+}
+Counter.prototype.decrease = function() {
+    this.value--;
+    this.valueDOM.textContent = this.value;
+}
+Counter.prototype.reset = function() {
+    this.value = 0;
+    this.valueDOM.textContent = this.value;
+}
+
+const firstCounter = new Counter(getElement('.first-counter'), 100)
+const secondCounter = new Counter(getElement('.second-counter'), 200)
+
 
