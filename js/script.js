a23a816f6e13df3abfb1f9336f9c1f0d29d42146
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-09-01 12:12:00 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 // window.addEventListener("DOMContentLoaded", firstCounterVar)
 // window.addEventListener("DOMContentLoaded", secondCounterVar)
-const firstCounter = document.querySelector('.first-counter .value');
-const secondCounter = document.querySelector('.second-counter .value');
+const firstCounterVal = document.querySelector('.first-counter .value');
+const secondCounterVal = document.querySelector('.second-counter .value');
 
 function Counter(element, value) {
     // console.log(element, value);
     element.textContent = value;
 }
 
-
-const firstCounterVar = new Counter(firstCounter, 100)
-const secondCounterVar = new Counter(secondCounter, 200)
+window.addEventListener("DOMContentLoaded", function() {
+    new Counter(firstCounterVal, 100)
+    new Counter(secondCounterVal, 200)
+})
 
